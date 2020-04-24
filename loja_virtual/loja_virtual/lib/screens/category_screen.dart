e9944708f043e735688c796aadf5189b82c2cc4d
Marcelo Junio Correa <marcelo.jcorrea@hotmail.com>
2020-04-23 21:49:03 -0300

    Criando Categoria dos produtos

--- loja_virtual/loja_virtual/lib/screens/category_screen.dart
+++ loja_virtual/loja_virtual/lib/screens/category_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/products_data.dart';
+import 'package:lojavirtual/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
 
@@ -23,13 +25,40 @@ class CategoryScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(//irá mostrar as guias dos Tabs
-          physics: NeverScrollableScrollPhysics(), //desativa a passagem de uma guia para outra pelo passar para o lado, força usar os icones
-          children: <Widget>[
-            Container(color: Colors.blue,),
-            Container(color: Colors.yellow),
-          ],
-        ),
+        body: FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance.collection("products").document(snapshot.documentID).collection('itens').getDocuments(),//irá buscar as informação da categoria que estamos, exemplo camisetas e o que está dentro de itens dele dentro do firestore
+          builder: (context, snapshot){
+            if(!snapshot.hasData)//se não retornar dados ficará o circulo rodando
+               return Center(child: CircularProgressIndicator(),);
+            else //se retornar os dados irá mostrar no grid ou lista do TabbarView
+              return
+                TabBarView(//irá mostrar as guias dos Tabs
+                  physics: NeverScrollableScrollPhysics(), //desativa a passagem de uma guia para outra pelo passar para o lado, força usar os icones
+                  children: <Widget>[
+                    GridView.builder(//como será exibido o grid na tela com os produtos
+                      padding: EdgeInsets.all(4.0),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(//como será exibido o grid na tela, formatação, espaçamento, quantidade de itens na grid
+                        crossAxisCount: 2, //qauntidade de itens na grid por linha
+                        childAspectRatio: 0.65, //razão da altura pela largura terá o aspectradio
+                        crossAxisSpacing: 4.0,// espaçamento do eixo principal
+                        mainAxisSpacing: 4.0, //espaçamento do eixo cruzado
+                      ),
+                      itemCount: snapshot.data.documents.length,//tamanho da grid
+                      itemBuilder: (context, index){//dados a exibir do firestore
+                        return ProductTile("grid", ProductData.fromDocument(snapshot.data.documents[index]));//irá buscar os dados da classe ProductTile, caso mude de banco, só iria precisar alterar o código nessa classe e não em todos os pontos que chamar o firestore
+                      },
+                    ),
+                    ListView.builder(//como será listado em lista os produtos
+                      padding: EdgeInsets.all(4.0),//espaçamento
+                      itemCount: snapshot.data.documents.length,//tamanho da lista
+                      itemBuilder: (context, index){//dados a exibir do firestore
+                        return ProductTile("list", ProductData.fromDocument(snapshot.data.documents[index]));//irá buscar os dados da classe ProductTile, caso mude de banco, só iria precisar alterar o código nessa classe e não em todos os pontos que chamar o firestore
+                      },
+                    )
+                  ],
+                );
+          }
+        )
       ),
     );
   }
---
+++ loja_virtual/loja_virtual/lib/tiles/product_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/products_data.dart';
+
+class ProductTile extends StatelessWidget {
+
+  final String type; //variavel type do tipo string
+  final ProductData data; //variavel do tipo ProductData
+
+  ProductTile(this.type, this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
